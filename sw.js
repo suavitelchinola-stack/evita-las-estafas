b2948f7dd8bc647bc92f373d25ef8ade74ee771a
suavitelchinola-stack <suavitelchinola@gmail.com>
2025-12-08 14:57:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "Evita Estafas · 100% Dominicana",
+  "short_name": "EvitaEstafas",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#071023",
+  "theme_color": "#0f1724",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
